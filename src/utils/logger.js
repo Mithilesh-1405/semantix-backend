f835242d974b39d598f7d66f1af615bb8a5c7bc2
mithilesh-1405 <mithileshmy14@gmail.com>
2026-05-30 13:22:40 +0530

    fix path

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+const path = require('path');
 
 const logger = winston.createLogger({
     level: process.env.LOG_LEVEL || 'info',
@@ -13,8 +14,8 @@ const logger = winston.createLogger({
         winston.format.json()
     ),
     transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        new winston.transports.File({ filename: path.join(__dirname, '../logs/error.log'), level: 'error' }),
+        new winston.transports.File({ filename: path.join(__dirname, '../logs/combined.log') })
     ]
 });
 
